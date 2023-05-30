7f17e60c93eb4484b24764cd12a909eb01af9bfd
悠人 菅原 <220369@jc-21.jp>
2023-05-30 11:34:09 +0900

    コンスタントバッファの作成まで

---
+++ Quad.h
@@ -0,0 +1,21 @@
+#pragma once
+
+//インクルード
+#include <DirectXMath.h>
+#include "Direct3D.h"
+
+using namespace DirectX;
+
+class Quad
+{
+	ID3D11Buffer* pVertexBuffer_;	//頂点バッファ
+	ID3D11Buffer* pIndexBuffer_;	//インデックスバッファ
+	ID3D11Buffer* pConstantBuffer_;	//コンスタントバッファ
+public:
+	Quad();
+	~Quad();
+	void Initialize();
+	void Draw();
+	void Release();
+};
+
