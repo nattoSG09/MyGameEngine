3ae53af3823545b02cfb5f24eb50023adc4c7ca2
悠人 菅原 <220369@jc-21.jp>
2023-05-31 12:31:19 +0900

    releaseとか

--- Direct3D.cpp
+++ Direct3D.cpp
@@ -144,13 +144,14 @@ void Direct3D::EndDraw()
 //‰ð•úˆ—
 void Direct3D::Release()
 {
-	pRasterizerState->Release();
-	pVertexLayout->Release();
-	pPixelShader->Release();
-	pVertexShader->Release();
+	SAFE_RELEASE(pRasterizerState);
+	SAFE_RELEASE(pVertexLayout);
+	SAFE_RELEASE(pPixelShader);
+	SAFE_RELEASE(pVertexShader);
 
-	pRenderTargetView->Release();
-	pSwapChain->Release();
-	pContext->Release();
-	pDevice->Release();
+	SAFE_RELEASE(pRenderTargetView);
+	SAFE_RELEASE(pSwapChain);
+	SAFE_RELEASE(pContext);
+	SAFE_RELEASE(pDevice);
+
 }
