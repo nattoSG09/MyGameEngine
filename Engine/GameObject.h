992e96719239926607930a5e6842f2985864a5c6
悠人 菅原 <220369@jc-21.jp>
2023-07-04 15:11:39 +0900

    faf

--- Engine/GameObject.h
+++ Engine/GameObject.h
@@ -1,5 +1,23 @@
 #pragma once
+
+#include œœœœœ
+#include "Fbx.h"
+#include "Transform.h"
+
 class GameObject
 {
-};
+	œœœœœœœœ childList_;
+	Transform	transform_;
+	GameObject*	pParent_;
+	string	objectName_;
 
+public:
+	GameObject();
+	GameObject(GameObject* parent, const std::string& name);
+	~GameObject();
+
+	virtual void Initialize() = 0;
+	virtual void Update() = 0;
+	virtual void Draw() = 0;
+	virtual void Release() = 0;
+};
